46cb584bad070143fd85d606f87dbf38079a9390
sungminstar <sungmin8453@naver.com>
2024-07-29 13:59:02 +0900

    feat: myPage 구현

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,10 +1,29 @@
-import {SafeAreaView, Text, View} from 'react-native';
-import {COLOR} from '../styles/color';
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import SettingSection from "../components/MyPage/SettingSection";
+import ProfileSection from "../components/MyPage/ProfileSection";
+import TabSection from "../components/MyPage/TabSection";
+import { COLOR } from "../styles/color";
 
 export default () => {
   return (
-    <SafeAreaView style={{flex: 1, backgroundColor: COLOR.WHITE}}>
-      <Text>마이페이지</Text>
+    <SafeAreaView style={styles.safeAreaView}>
+      <View style={styles.container}>
+        <SettingSection />
+        <ProfileSection />
+        <TabSection />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    backgroundColor: COLOR.WHITE,
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    width: "100%",
+  },
+});
