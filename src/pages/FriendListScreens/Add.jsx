d1df97848a13cfb35b5307d94db7fc2701a619f3
moko <ljun925@naver.com>
2024-07-30 22:45:24 +0900

    fixed: image uri 경로 변경

--- src/pages/FriendListScreens/Add.jsx
+++ src/pages/FriendListScreens/Add.jsx
@@ -94,7 +94,7 @@ export default function FriendSearchScreen() {
     return (
       <View style={styles.renderItemContainer}>
         <View style={styles.renderItemProfile}>
-          <Image source={item.image || cat} style={styles.renderItemImage} />
+          <Image source={{ uri: item.image }} style={styles.renderItemImage} />
           <Text style={styles.nickname}>{item.nickname}</Text>
         </View>
         <TouchableOpacity
